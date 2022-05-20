f5d93fca23fbf8afcf7f815628d0eb768392979f
king <836334258@qq.com>
2022-05-20 10:32:27 +0800

    优化显示逻辑

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,32 @@
-import { defineUserConfig } from 'vuepress'
+import { defaultTheme, defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'zh-CN',
-  title: '你好， VuePress ！',
-  description: '这是我的第一个 VuePress 站点',
+  title: 'King随笔',
+  description: '随笔',
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/836334258/',
+      },
+    ],
+    sidebar: [
+      '/README.md',
+      {
+        text: 'Code',
+        link: '/code',
+        children: [
+          {
+            text: '前端零碎知识点',
+            link: '/code/knowledge.md',
+          },
+        ],
+      },
+    ],
+  }),
 })
